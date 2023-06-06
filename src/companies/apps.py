71ac684b42c61c639dcf9ea66b7d1229174e03d5
Pedro Ferrari <pdrferrari08@gmail.com>
2023-06-06 18:42:45 -0300

    feat: company feature + refactor folder structure

---
+++ src/companies/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class CompaniesConfig(AppConfig):
+    """Company app configuration class"""
+
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "companies"
