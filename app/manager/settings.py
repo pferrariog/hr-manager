b1c20cc71b487834441e15ac5170420ea0c2fb87
Pedro Ferrari <pdrferrari08@gmail.com>
2023-06-08 16:54:14 -0300

    feat: dynaconf added and changed folder name

--- app/manager/settings.py
+++ app/manager/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+from dynaconf import DjangoDynaconf
+
+
+# Dynaconf Settings
+settings = DjangoDynaconf(__name__)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = "asd"
+SECRET_KEY = settings.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = settings.get("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = settings.get("ALLOWED_HOSTS")
 
 
 # Application definition
