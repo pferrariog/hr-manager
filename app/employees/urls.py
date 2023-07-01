bbaa22353c3449a66a5b0312247b5e5321e7024d
Pedro Ferrari <pdrferrari08@gmail.com>
2023-07-01 14:39:05 -0300

    feat: create and delete employees

--- app/employees/urls.py
+++ app/employees/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
+from .views import CreateEmployee
+from .views import DeleteEmployee
 from .views import EditEmployee
 from .views import ListEmployees
 
 
 urlpatterns = [
     path("", ListEmployees.as_view(), name="employees_information"),
+    path("new/", CreateEmployee.as_view(), name="create_employee"),
     path("edit/<int:id>/", EditEmployee.as_view(), name="edit_employee"),
+    path("delete/<int:id>/", DeleteEmployee.as_view(), name="delete_employee"),
 ]
--- app/employees/views.py
+++ app/employees/views.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import User
+from django.forms.models import BaseModelForm
+from django.http import HttpResponse
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.views.generic import DeleteView
 from django.views.generic.edit import UpdateView
 from django.views.generic.list import ListView
 
@@ -21,3 +27,28 @@ class EditEmployee(UpdateView):
     model = Employee
     fields = ["name", "departments"]
     pk_url_kwarg = "id"
+
+
+class DeleteEmployee(DeleteView):
+    """Default delete employee view"""
+
+    model = Employee
+    pk_url_kwarg = "id"
+    success_url = reverse_lazy("employees_information")
+
+
+class CreateEmployee(CreateView):
+    """Default create employee view"""
+
+    model = Employee
+    fields = ["name", "departments"]
+
+    def form_valid(self, form: BaseModelForm) -> HttpResponse:
+        """Override base form_valid function"""
+        employee_object = form.save(commit=False)
+        employee_object.company = self.request.user.employee.company
+        employee_object.user = User.objects.create(  # TODO add field to model form
+            username="".join(employee_object.name.split(" "))
+        )
+        employee_object.save()
+        return super(CreateEmployee, self).form_valid(form)
